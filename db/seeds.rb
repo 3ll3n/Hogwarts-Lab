21aafe2d5e637127f675b21cba2dd17b48ce6df2
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-02-01 16:05:32 +0000

    Added houses table and created house.rb and added save and find

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require('pry-byebug')
 require_relative("../models/student")
+require_relative("../models/house")
 
 student1 = Student.new({"first_name" => "Harry", "second_name" => "Potter", "house" => "Gryffindor", "age" => 13})
 student2 = Student.new({"first_name" => "Ron", "second_name" => "Weasley", "house" => "Gryffindor", "age" => 13})
@@ -8,6 +9,11 @@ student3 = Student.new({"first_name" => "Draco", "second_name" => "Malfoy", "hou
 student1.save
 student2.save
 student3.save
+
+house1 = House.new({"name" => "Gryffindor"})
+house2 = House.new({"name" => "Slytherin"})
+house3 = House.new({"name" => "Ravenclaw"})
+house4 = House.new({"name" => "Hufflepuff"})
 
 binding.pry
 
--- db/students.sql
+++ db/students.sql
@@ -1,4 +1,5 @@
 DROP TABLE students;
+DROP TABLE houses;
 
 CREATE TABLE students (
   id SERIAL4 primary key,
@@ -6,4 +7,10 @@ CREATE TABLE students (
   second_name VARCHAR(255) not null,
   house VARCHAR(255) not null,
   age INT4 not null
+);
+
+CREATE TABLE houses (
+  id SERIAL4 primary key,
+  name VARCHAR(255) not null,
+  logo_url VARCHAR(255) not null
 );
